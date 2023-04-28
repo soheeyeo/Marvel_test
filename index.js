d0dd87473cb0a02c0cae56d232350b1af6734a49
soheeyeo <sohee407@gmail.com>
2023-04-28 10:30:29 +0900

    Quiz 01

---
+++ index.js
@@ -0,0 +1,5 @@
+// 타입스크립트 기본 타입
+// 01. 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
+var userName = 'yeo';
+var age = 30;
+var area = 'korea';
---
+++ index.ts
@@ -0,0 +1,5 @@
+// 타입스크립트 기본 타입
+// 01. 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
+let userName :string = 'yeo';
+let age :number = 30;
+let area :string = 'korea';
