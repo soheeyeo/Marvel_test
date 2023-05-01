7cd9a0d44fc633f02464e0a9e195a004d5ebf4e5
soheeyeo <sohee407@gmail.com>
2023-05-01 10:50:11 +0900

    Update quiz03-03

--- quiz03.js
+++ quiz03.js
@@ -16,3 +16,21 @@ function 함수(x) {
 function 함수(x) {
     return x.toString().length;
 }
+// 03. 결혼 가능 확률을 알려주는 함수를 만들어봅시다.
+// 1. 함수의 파라미터로 월소득(만원단위), 집보유여부(true/false), 매력점수 ('상' or '중' or '하') 를 입력할 수 있어야합니다.
+// 2. 월소득은 만원 당 1점, 집보유시 500점 & 미보유시 0점, 매력점수는 '상'일 때만 100점으로 계산합니다.
+// 3. 총 점수가 600점 이상일 경우 "결혼가능"을 return 해줘야합니다. 그 외엔 아무것도 return하지 않습니다.
+function canMarry(월소득, 집, 매력) {
+    var score = 0;
+    score += 월소득;
+    if (집 === true) {
+        score += 500;
+    }
+    if (매력 === '상') {
+        score += 100;
+    }
+    if (score >= 600) {
+        return '결혼가능';
+    }
+}
+console.log(canMarry(100, true, '상'));
