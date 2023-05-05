4706145fb275773fbc229a66f0d0d707726b6291
soheeyeo <sohee407@gmail.com>
2023-05-05 10:30:11 +0900

    Update quiz04-02

--- quiz04.ts
+++ quiz04.ts
@@ -23,3 +23,32 @@ function cleaning(x: (number|string)[]) {
 }
 
 console.log(cleaning([123,'3']));
+
+// 02. 다음과 같은 함수를 만들어보십시오.
+// let 철수쌤 = { subject : 'math' }
+// let 영희쌤 = { subject : ['science', 'english'] }
+// let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장이 되어있습니다.
+// 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되어있고
+// 과목 2개 이상 가르치는 쌤들은 array 자료로 과목들이 저장되어있습니다.
+// 철수쌤같은 선생님 object 자료를 집어넣으면
+// 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+// 그리고 타입지정도 엄격하게 해보도록 합시다.
+// (동작예시)
+
+// 만들함수( { subject : 'math' } )//이 경우 'math'를 return
+// 만들함수( { subject : ['science', 'art', 'korean'] } )//이 경우 'korean'을 return
+// 만들함수( { hello : 'hi' } )//이 경우 타입에러 나면 됩니다
+
+function 함수(x: {subject : string | string[]}) {
+	if(typeof x.subject === 'string') {
+		return x.subject;
+	} else if (Array.isArray(x.subject)) {
+		return x.subject[x.subject.length - 1];
+	} else {
+		return '에러';
+	}
+}
+
+console.log(함수({subject : ['english', 'art']}));
