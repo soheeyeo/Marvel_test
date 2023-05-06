cdd670f5c64b902a87b7941bd54314b4949e1d5b
soheeyeo <sohee407@gmail.com>
2023-05-06 11:11:28 +0900

    Update quiz05-02

---
+++ quiz05.ts
@@ -0,0 +1,15 @@
+// 01. object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면 어떻게 될까요?
+
+// 02. 1. **다음 조건을 만족하는 타입을 만들어봅시다.**
+    
+// 1. 이 타입은 object 자료형이어야합니다.
+// 2. 이 타입은 color 라는 속성을 가질 수도 있으며 항상 문자가 들어와야합니다.
+// 3. 이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
+// 4. 이 타입은 position 이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야합니다.
+// type alias로 만들어보셈
+
+type Design = {
+	color? : string, 
+	size : number,
+	readonly position : number[]
+}
