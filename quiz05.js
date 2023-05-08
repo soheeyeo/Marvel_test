2d2edceba535c360ec35b26c37354d360da05124
soheeyeo <sohee407@gmail.com>
2023-05-08 10:55:11 +0900

    Update quiz05-03

--- quiz05.js
+++ quiz05.js
@@ -1,1 +1,6 @@
 // 01. object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면 어떻게 될까요?
+var info = {
+    name: 'kim',
+    phone: 123,
+    email: 'abc@naver.com'
+};
--- quiz05.ts
+++ quiz05.ts
@@ -13,3 +13,17 @@ type Design = {
 	size : number,
 	readonly position : number[]
 }
+
+// 03. **다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오.**
+    
+//     1. 대충 이렇게 생긴 object 자료를 다룰 일이 많습니다. **{ name : 'kim', phone : 123, email : 'abc@naver.com' }**
+//     2. object 안에 있는 **이름, 전화번호, 이메일** 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
+//     3. 각 속성이 어떤 타입일지는 자유롭게 정하십시오.
+
+type User = {name :string, phone :number, email :string};
+
+let info :User = {
+	name : 'kim',
+	phone : 123,
+	email :'abc@naver.com' 
+}
