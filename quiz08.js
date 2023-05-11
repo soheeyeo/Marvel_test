dec2cdf43ddd97859dbc096339298d10128a9a07
soheeyeo <sohee407@gmail.com>
2023-05-11 10:45:20 +0900

    Update quiz08-02

--- quiz08.js
+++ quiz08.js
@@ -15,3 +15,32 @@ var Car = /** @class */ (function () {
     };
     return Car;
 }());
+// 02. **class인데 파라미터가 잔뜩 들어가는 class Word를 만들어봅시다.**
+// 1. object 만들 때 new Word() 소괄호 안에 숫자 혹은 문자를 입력하면
+// 2. 숫자는 전부 object 안의  num 속성 안에 array 형태로 저장되고
+// 3. 문자는 전부 object 안의 str 속성 안에 array 형태로 저장되는 class를 만들어봅시다.
+// 4. class 만들 때 넣을 수 있는 숫자와 문자 갯수는 일단 제한은 없습니다. 그리고 타입 빼먹지 마셈
+// (동작 예시)
+// let obj = new Word('kim', 3, 5, 'park');
+// console.log(obj.num)//[3,5]console.log(obj.str)//['kim', 'park']
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var numbers = [];
+        var words = [];
+        param.forEach(function (i) {
+            if (typeof i === 'string') {
+                words.push(i);
+            }
+            else {
+                numbers.push(i);
+            }
+        });
+        this.num = numbers;
+        this.str = words;
+    }
+    return Word;
+}());
