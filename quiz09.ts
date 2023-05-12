265c6f0e13851bb43eb4f6d51d2f6eff7b7b22ee
soheeyeo <sohee407@gmail.com>
2023-05-12 14:32:11 +0900

    Update quiz09-03

--- quiz09.ts
+++ quiz09.ts
@@ -35,3 +35,21 @@ interface Cart {
 
 let 장바구니 :Cart[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
 
+// 03. **위에서 만든 타입을 extends 해봅시다.**
+
+// 갑자기 서비스가 업데이트되어서 일부 상품은 card 속성이 들어가야합니다.
+
+// ```
+// { product : '청소기', price : 7000, card : false }
+// ```
+
+// 위에서 만든 interface를 extends 해서 이 object의 타입을 만들어보십시오.
+
+interface Cart {
+	product :string;
+	price :number;
+}
+
+interface NewCart extends Cart {
+	card :boolean;
+}
