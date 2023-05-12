dd9c4b2d6ca43bbeadf1a02bd585c067c79b6639
soheeyeo <sohee407@gmail.com>
2023-05-12 14:45:02 +0900

    Update quiz09-04

--- quiz09.ts
+++ quiz09.ts
@@ -53,3 +53,27 @@ interface Cart {
 interface NewCart extends Cart {
 	card :boolean;
 }
+
+// 04. **object 안에 함수를 2개 넣고 싶은데요**
+
+// 1. 이 object 자료는 plus() 함수를 내부에 가지고 있으며 plus 함수는 파라미터 2개를 입력하면 더해서 return 해줍니다.
+
+// 2. 이 object 자료는 minus() 함수를 내부에 가지고 있으며 minus 함수는 파라미터 2개를 입력하면 빼서 return 해줍니다.
+
+// 이 object 자료를 어떻게 만들면 될까요?
+
+// interface를 이용해서 object에 타입지정도 해보십시오.
+
+interface MathObj {
+	plus :(a :number, b:number) => number;
+	minus :(a :number, b:number) => number;
+}
+
+let Obj :MathObj = {
+	plus(a, b) {
+		return a + b;
+	},
+	minus(a, b) {
+		return a - b;
+	}
+}
