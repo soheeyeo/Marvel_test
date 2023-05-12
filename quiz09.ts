941adc871a77abfb426fd459ced90d4b26ee42fb
soheeyeo <sohee407@gmail.com>
2023-05-12 13:07:21 +0900

    Update quiz09-02

--- quiz09.ts
+++ quiz09.ts
@@ -15,3 +15,23 @@ interface Item {
 }
 
 let 상품 :Item = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+
+// 02. **array 안에 object 여러개가 필요합니다.**
+
+// 쇼핑몰 장바구니를 구현하려고 하는데
+
+// ```
+// let 장바구니 = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+// ```
+
+// 이렇게 생긴 object들이 잔뜩 들어갈 수 있는 array는 어떻게 타입을 지정해야할까요?
+
+// 오늘 배운 interface 문법을 써봅시다.
+
+interface Cart {
+	product :string;
+	price :number;
+}
+
+let 장바구니 :Cart[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+
