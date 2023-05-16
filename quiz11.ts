d721f0f3f62d1c238639c30013ebbf8e251552fb
soheeyeo <sohee407@gmail.com>
2023-05-16 17:26:10 +0900

    Update quiz11-01

---
+++ quiz11.ts
@@ -0,0 +1,22 @@
+// 01. **다음 x, y, z 속성의 특징을 설명해보십시오.**
+
+// ```
+// class User {
+//   private static x = 10;
+//   public static y = 20;
+//   protected z = 30;
+// }
+// ```
+
+// 누가 쓸 수 있고, 어디서 수정할 수 있는지 이런 것들이요.
+
+// 친구가 물어봤을 때 어떻게 답해줄 것입니까
+
+x와 y에 static 키워드가 붙었기 때문에 User.x 이런 식으로만 접근해서 쓸 수 있음. User의 자식들은 x, y 쓸 수 없음 
+
+private static x : 부모 class{}에서만 사용 가능. 자식에게 안 물려줌
+
+public static y : class 내부 외부 상관없이 수정 가능. public 키워드 지워도 똑같이 동작 
+
+protected z : class{}에서 사용 가능. 현재 class, extends된 class 내에서 사용 가능
+
