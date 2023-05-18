eb8cd9ce3f6d30ac4dde036b198b042327f33b6e
soheeyeo <sohee407@gmail.com>
2023-05-18 15:43:49 +0900

    Update quiz13-03

--- quiz13.js
+++ quiz13.js
@@ -14,3 +14,27 @@ function makeObj(x) {
 var data = '{"name" : "dog", "age" : 1 }';
 var a = makeObj(data);
 console.log(a);
+// 03. **class 를 수정해봅시다.**
+// ```
+// class Person {
+//   name;
+//   constructor(a){
+//     this.name = a;
+//   }
+// }
+// let a = new Person('어쩌구');
+// a.name//any 타입이 되었넹
+// ```
+// 지금 만든 class는 new Person('어쩌구') 라고 분명 문자를 집어넣었는데 any 타입이 name 속성에 부여됩니다.
+// 이게 싫어서 파라미터에 string을 집어넣으면 string 타입
+// number를 집어넣으면 number 타입
+// string[]을 집어넣으면 string[] 타입이 되게 하려면 위의 코드를 어떻게 수정해야할까요?
+// 오늘 배운 Generic을 이용해봅시다.
+var Person = /** @class */ (function () {
+    function Person(a) {
+        this.name = a;
+    }
+    return Person;
+}());
+var b = new Person('어쩌구');
+b.name;
