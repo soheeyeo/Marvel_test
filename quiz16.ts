74f6030c2ca8890323f90c621ef3d843d79ddce4
soheeyeo <sohee407@gmail.com>
2023-05-25 10:07:21 +0900

    Update quiz16-02

--- quiz16.ts
+++ quiz16.ts
@@ -27,3 +27,23 @@ type TypeChanger<MyType> = {
 }
 
 type 새로운타입 = TypeChanger<Bus>;
+
+// 02. **이런 변환기는 어떻게 만들어야할까요?**
+
+// object안에 들어있는 모든 속성을
+
+// string, number 이렇게 고정된 타입으로 변환해주는게 아니라
+
+// 내가 원하는 타입을 입력하면 그걸로 변환해주는 범용성 좋은 변환기를 만들어보십시오.
+
+type Bus2 = {
+    color : string,
+    model : boolean,
+    price : number
+}
+
+type TypeChanger2<MyType, T> = {
+    [key in keyof MyType] :T;
+}
+
+type NewBus = TypeChanger2<Bus2, boolean>;
